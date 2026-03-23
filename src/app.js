a7da4ef2585b685b6bc9a61fcee46c71ccbfbbbb
Vedant Prashant Shende <btech10662.24@bitmesra.ac.in>
2026-03-23 21:06:26 +0530

    setting up done

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+//basic configuration
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+
+//cors configugration
+app.use(cors({
+    origin: process.env.CORS_ORGIN?.split(",") || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+app.get("/", (req, res) => {
+    console.log("Hello This is TaskFlow AI");
+    res.send("Hello This is TaskFlow AI");
+})
+
+export default app;
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,10 @@
+class APIResponse {
+    constructor(statusCode, message, data = "Success") {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = true;
+    }
+}
+
+export default APIResponse;
